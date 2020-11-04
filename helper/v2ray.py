eea1723991521852756988c59f5971d939f2846a
who8736 <who8736@hotmail.com>
2020-11-04 17:19:04 +0800

    增加v2ray验证

--- helper/v2ray.py
+++ helper/v2ray.py
@@ -1,4 +1,5 @@
 import requests
+import socket
 from base64 import b64decode, b64encode
 import json
 import os
@@ -44,39 +45,51 @@ def readFile():
             yield node
 
 
-def genV2rayConifg(outfilename, inport=1080):
-    filename = os.path.join(ROOT_PATH, 'v2raysample.json')
-    with open(filename, 'r', encoding='utf8') as f:
+def genV2rayConifg(node, filename, inport=1080):
+    samplefilename = os.path.join(ROOT_PATH, 'v2raysample.json')
+    with open(samplefilename, 'r', encoding='utf8') as f:
         config = json.load(f)
 
-    node = list(readFile())[0]
-
+    # 配置代理出口
     vnext = [{
         'address': node['add'],
         'port': int(node['port']),
-        'users': [
-            {
-                'id': node['id'],
-                'alterId': int(node['aid']),
-                'email': 'f@f.ff',
-                'security': 'auto',
-                'encryption': None,
-                'flow': None
-            }
-        ]
+        'users': [{
+            'id': node['id'],
+            'alterId': int(node['aid']),
+            'email': 'f@f.ff',
+            'security': 'auto',
+            'encryption': None,
+            'flow': None
+        }]
     }]
-    config['outbounds'][0]['settings']['vnext'] = vnext
-    config['outbounds'][0]['streamSettings']['network'] = node['net']
-    config['outbounds'][0]['streamSettings']['security'] = node['tls']
-    config['outbounds'][0]['streamSettings']['tlsSettings']['serverName'] = node[
-        'host']
-    config['outbounds'][0]['streamSettings']['wsSettings']['path'] = node['path']
-    config['outbounds'][0]['streamSettings']['wsSettings']['headers']['Host'] = node['host']
+    outbound = config['outbounds'][0]
+    outbound['settings']['vnext'] = vnext
+    outstream = outbound['streamSettings']
+    outstream['network'] = node['net']
+    outstream['security'] = node['tls']
+    outstream['tlsSettings']['serverName'] = node['host']
+    outstream['wsSettings']['path'] = node['path']
+    outstream['wsSettings']['headers']['Host'] = node['host']
+    outbound['streamSettings'] = outstream
+    config['outbounds'][0] = outbound
+
+    # 配置代理入口
     config['inbounds'][0]['port'] = inport
+
     # print(config)
-    with open(outfilename, 'w', encoding='utf8') as f:
-        print('save file:', outfilename)
+    with open(filename, 'w', encoding='utf8') as f:
+        print('save file:', filename)
         json.dump(config, f)
+
+
+def getV2rayPort(startPort=26000, stopPort=27000):
+    sock = socket.socket()
+    sock.bind(('', 0))
+    ip, port = sock.getnameinfo()
+    print(port)
+    sock.close()
+    return port
 
 
 if __name__ == '__main__':
@@ -85,25 +98,17 @@ if __name__ == '__main__':
     # print('-' * 80)
     # print(a)
 
-    filename = os.path.join(ROOT_PATH, 'config1.json')
-    print('save file:', filename)
-    genV2rayConifg(filename, inport=10803)
+    # 生成配置文件
+    # filename = os.path.join(ROOT_PATH, 'config1.json')
+    # print('save file:', filename)
+    # node = list(readFile())[0]
+    # genV2rayConifg(node, filename=filename, inport=10803)
 
     # url = 'ew0KICAidiI6ICIyIiwNCiAgInBzIjogImdpdGh1Yi5jb20vZnJlZWZxIC0g576O5Zu95YaF5Y2O6L6+5bee5ouJ5pav57u05Yqg5pavQnV5Vk0yMjkiLA0KICAiYWRkIjogIm1nYS5jZW50b3M4LmNsb3VkIiwNCiAgInBvcnQiOiAiNDQzIiwNCiAgImlkIjogIjliZjBiMmExLWY4OTAtM2Q3OS1iYTBiLTBhYTM1YTA0ZDhkNSIsDQogICJhaWQiOiAiMTYiLA0KICAibmV0IjogIndzIiwNCiAgInR5cGUiOiAibm9uZSIsDQogICJob3N0IjogIm1nYS5jZW50b3M4LmNsb3VkIiwNCiAgInBhdGgiOiAiL21vdmllIiwNCiAgInRscyI6ICJ0bHMiDQp9'
     # nodeStr = b64decode(url).decode('utf8')
     # node = json.loads(nodeStr)
     # print(node)
 
-    # a = {
-    #     'v': '2',
-    #     'ps': 'github.com/freefq - 美国内华达州拉斯维加斯BuyVM229',
-    #     'add': 'mga.centos8.cloud',
-    #     'port': '443',
-    #     'id': '9bf0b2a1-f890-3d79-ba0b-0aa35a04d8d5',
-    #     'aid': '16',
-    #     'net': 'ws',
-    #     'type': 'none',
-    #     'host': 'mga.centos8.cloud',
-    #     'path': '/movie',
-    #     'tls': 'tls'
-    # }
+    # 获取空端口
+    getV2rayPort()
+
